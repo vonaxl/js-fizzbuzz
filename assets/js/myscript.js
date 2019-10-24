5c854b6981873376cd970b82b01ebbafc74c2dc2
vonaxl <vonaxlcorso@gmail.com>
2019-10-24 17:05:07 +0200

    push

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -23,3 +23,23 @@ for (var i = 1; i < 101; i++) {
   b = document.getElementById('risultato').innerHTML;
   document.getElementById('risultato').innerHTML = b + "<li>"+a+"</li>";
 }
+
+// FIZZ BIZZ USING WHILE
+
+// c=1;
+// while (c<101) {
+//   if (c % 15 == 0) {
+//       console.log("FizzBuzz");
+//       a = "FizzBuzz";
+//     } else if (c % 5 == 0) {
+//       console.log("Buzz");
+//       a = "Buzz";
+//     }else if(c % 3 == 0){
+//       console.log("Fizz");
+//       a = "Fizz";
+//     }else {
+//       console.log(c);
+//       a = c;
+//     }
+//   c++;
+// }
